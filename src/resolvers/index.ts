fe101691cefeb12eeaa62af63189c502caca4a07
Rohail Malhi <XP.Rohail.Malhi@inovalon.global>
2021-08-18 03:49:45 +0500

    Adding schema and resolver for launches

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -4,6 +4,9 @@ import { IResolvers } from "graphql-tools/dist/Interfaces"
 
 export const resolvers: IResolvers = {
     Query: {
-        hello: (root: any, args: any, context: any) => hello(args, context)
+        // hello: (root: any, args: any, context: any) => hello(args, context)
+        launches: (root: any, args: any, context: any) => hello(args, context),
+        launch: (root: any, args: any, context: any) => hello(args, context),
+        me: (root: any, args: any, context: any) => hello(args, context)
     }
 }
--- src/schema.ts
+++ src/schema.ts
@@ -1,14 +1,55 @@
+
+
 const schema = `
 
-
-
-"""
-A hello world Query
-"""
-type Query {
-     hello: String!
+type Rocket {
+     id: ID!
+     name: String
+     type: String
 }
 
+type Mission {
+     id: ID!
+     missionPatch(size: PatchSize): String 
+}
+
+type Launch {
+     id: ID!
+     site: String
+     mission: Mission
+     rocket: Rocket
+     isBooked: Boolean!
+}
+
+type User {
+     id: ID!
+     email: String
+     trips: [Launch]
+     token: String 
+}
+
+enum PatchSize {
+     SMALL
+     LARGE
+}
+
+type Query {
+     launches: [Launch]
+     launch(id: ID!): Launch 
+     me: User
+}
+
+type Mutation {
+     bookTrips(launchIds: [ID]!): TripUpdatedResponse!
+     cancelTrip(launchIds: [ID]!): TripUpdatedResponse!
+     login(email: String): User
+}
+
+type TripUpdatedResponse {
+     success: Boolean!
+     message: String
+     launches: [Launch]
+}
 `
 
 export { schema }
